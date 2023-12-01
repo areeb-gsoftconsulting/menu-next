ca58e88db284aadbf0524fb9d3fe3693abcb997e
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-12-01 10:15:16 +0500

    feat: theming

--- src/pages/MenuPage/index.tsx
+++ src/pages/MenuPage/index.tsx
@@ -5,18 +5,24 @@ import {
   IonContent,
   IonGrid,
   IonHeader,
+  IonIcon,
   IonImg,
   IonItem,
+  IonLabel,
+  IonList,
   IonPage,
   IonRow,
   IonText,
   IonTitle,
+  IonToggle,
 } from "@ionic/react";
 import "./styles.css";
 import bannerImage from "../../assets/bannerImage.png";
 import menuImg from "../../assets/menuImg.png";
 
 const Menu = () => {
+  // const toggleDarkModeHandler = () => document.body.classList.toggle("dark");
+
   return (
     <IonPage className="page">
       <IonHeader className="ion-no-border header" translucent={true}>
@@ -29,6 +35,17 @@ const Menu = () => {
         <IonText className="selectMenu">
           <h2>Select Menu</h2>
         </IonText>
+        {/* <IonList>
+          <IonItem lines="none">
+            <IonIcon slot="start" />
+            <IonLabel>Dark Mode</IonLabel>
+            <IonToggle
+              slot="end"
+              name="darkMode"
+              onIonChange={toggleDarkModeHandler}
+            />
+          </IonItem>
+        </IonList> */}
 
         <IonCol>
           <IonImg className="menuImg" src={menuImg} />
