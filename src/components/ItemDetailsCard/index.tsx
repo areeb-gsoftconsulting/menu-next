8ede021c391ef67fc3dbfa00fcab2e4a5f0f3d30
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-12-05 17:14:15 +0500

    feat: added card btn

--- src/components/ItemDetailsCard/index.tsx
+++ src/components/ItemDetailsCard/index.tsx
@@ -16,6 +16,9 @@ import {
   IonCol,
   IonRadioGroup,
   IonRadio,
+  IonCheckbox,
+  IonTextarea,
+  IonButton,
 } from "@ionic/react";
 import thumbnailImg from "../../assets/menuImg.png";
 import { starSharp } from "ionicons/icons";
@@ -100,6 +103,70 @@ const ItemDetailsCard = ({ isOpen, setIsOpen }: any) => {
             </IonRow>
           </IonRadioGroup>
         </div>
+
+        <div className={styles.optionalCard}>
+          <IonRow
+            className={`ion-justify-content-between ion-align-items-center`}
+          >
+            <h4 className={`${styles.caption} ion-no-padding`}>
+              Choice of Toppings
+            </h4>
+            <h4 className={`${styles.optional}`}>Optional</h4>
+          </IonRow>
+          <p className={styles.msg}>Select upto 2</p>
+          <IonRow
+            className={`ion-justify-content-between ion-align-items-center`}
+          >
+            <IonCheckbox className={styles.checkBox} labelPlacement="end">
+              <p className={`${styles.priceLabel}`}>Less Toppings</p>
+            </IonCheckbox>
+            <p className={`${styles.priceLabel}`}>free</p>
+          </IonRow>
+          <IonRow
+            className={`ion-justify-content-between ion-align-items-center`}
+          >
+            <IonCheckbox className={styles.checkBox} labelPlacement="end">
+              <p className={`${styles.priceLabel}`}>Chrysanthemum</p>
+            </IonCheckbox>
+            <p className={`${styles.priceLabel}`}>$ 4</p>
+          </IonRow>
+          <IonRow
+            className={`ion-justify-content-between ion-align-items-center`}
+          >
+            <IonCheckbox className={styles.checkBox} labelPlacement="end">
+              <p className={`${styles.priceLabel}`}>Pink Cactus Pearls</p>
+            </IonCheckbox>
+            <p className={`${styles.priceLabel}`}>$ 4</p>
+          </IonRow>
+        </div>
+        <div className={styles.optionalCard}>
+          <h4 className={`${styles.caption} ion-no-padding`}>
+            Choice of Toppings
+          </h4>
+          <p className={styles.note}>
+            (Please let us know if you are allergic to anything or if we need to
+            avoid anything)
+          </p>
+          <IonTextarea
+            className={styles.textarea}
+            placeholder="eg. no mayo"
+            autoGrow={true}
+            rows={4}
+            fill="outline"
+          ></IonTextarea>
+        </div>
+        <IonRow
+          className={`ion-justify-content-between ion-align-items-center ion-padding-vertical`}
+        >
+          <IonRow
+            className={`ion-justify-content-evenly ion-align-items-center ${styles.leftRow}`}
+          >
+            <IonButton className={styles.actionBtn}>-</IonButton>
+            <h3>9</h3>
+            <IonButton className={styles.actionBtn}>+</IonButton>
+          </IonRow>
+          <IonButton className={styles.addBtn}>Add to Cart</IonButton>
+        </IonRow>
       </IonContent>
     </IonModal>
   );
