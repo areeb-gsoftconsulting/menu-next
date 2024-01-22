345617ee39a90ee163374462d2446b3f01b0789f
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2024-01-22 16:16:07 +0500

    feat: card fixed

--- src/pages/WelcomePage/index.tsx
+++ src/pages/WelcomePage/index.tsx
@@ -11,9 +11,16 @@ import getVenues from "../../services/getVenue";
 import { useDispatch } from "react-redux";
 import { setCurrentMenu, setVenue } from "../../store/slices/restaurantSlice";
 import { useIonRouter } from "@ionic/react";
+import { useLocation, useParams } from "react-router";
 
 const WelcomePage: React.FC = () => {
   const router = useIonRouter();
+  const location = useLocation();
+  const pathname = location.pathname;
+  const pathParts = pathname.split("/");
+  const slug = pathParts[1];
+  console.log("Requested slug:", slug);
+
   const dispatch = useDispatch();
   const getVlenue = async () => {
     try {
