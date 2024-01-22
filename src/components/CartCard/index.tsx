60f8281ee8db2e562489f51b50bc432678b2e7b8
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2024-01-22 13:51:37 +0500

    feat: added cart card action btn

--- src/components/CartCard/index.tsx
+++ src/components/CartCard/index.tsx
@@ -3,11 +3,82 @@ import React from "react";
 import itemImg from "../../assets/menuImg.png";
 import styles from "./styles.module.css";
 import { remove, add } from "ionicons/icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setCart, setCartItems } from "../../store/slices/cartSlice";
 
 const CartCard = ({ item }: any) => {
   console.log({ item });
   const venue = useSelector((data: any) => data.restaurant.venue);
+  const cart = useSelector((data: any) => data.cart.items);
+  const dispatch = useDispatch();
+
+  const addToCart = (param: any) => {
+    function arraysEqual(arr1, arr2) {
+      if (arr1.length !== arr2.length) {
+        return false;
+      }
+
+      for (let i = 0; i < arr1.length; i++) {
+        if (!objectsEqual(arr1[i], arr2[i])) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    // Function to compare objects
+    function objectsEqual(obj1, obj2) {
+      const keys1 = Object.keys(obj1);
+      const keys2 = Object.keys(obj2);
+
+      if (keys1.length !== keys2.length) {
+        return false;
+      }
+
+      for (let key of keys1) {
+        if (obj1[key] !== obj2[key]) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    // /////////////
+    let tempCart = [...cart];
+    let tempItemIndex = tempCart.findIndex(
+      (item: any) =>
+        item.id == param.id &&
+        item.price._id == param.price._id &&
+        arraysEqual(item.customization, param.customization)
+    );
+    if (tempItemIndex == -1 && param.quantity > 0) {
+      dispatch(setCartItems(param));
+    } else {
+      let updatedItem = { ...tempCart[tempItemIndex] };
+      updatedItem.quantity = updatedItem.quantity + param.quantity;
+
+      // Check if quantity becomes zero, remove the item from the cart
+      if (updatedItem.quantity === 0) {
+        tempCart.splice(tempItemIndex, 1); // Remove the item from the array
+      } else {
+        tempCart[tempItemIndex] = updatedItem;
+      }
+
+      dispatch(setCart(tempCart));
+    }
+
+    // let cartData: any = {
+    //   id: param.id,
+    //   name: param.name,
+    //   price: param.price,
+    //   customization: selectedCustomization,
+    //   comments: param.comments,
+    //   image: param.image,
+    // };
+    // dispatch(setCartItems(cartData));
+  };
 
   return (
     <>
@@ -55,6 +126,17 @@ const CartCard = ({ item }: any) => {
           <IonButton
             size="small"
             className={`${styles.iconBtn} ion-no-padding`}
+            onClick={() =>
+              addToCart({
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                customization: item.customization,
+                comments: item.comments,
+                image: item.image,
+                quantity: -1,
+              })
+            }
           >
             <IonIcon
               className={styles.icons}
@@ -67,6 +149,17 @@ const CartCard = ({ item }: any) => {
           <IonButton
             size="small"
             className={`${styles.iconBtn} ion-no-padding`}
+            onClick={() =>
+              addToCart({
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                customization: item.customization,
+                comments: item.comments,
+                image: item.image,
+                quantity: 1,
+              })
+            }
           >
             <IonIcon
               className={styles.icons}
