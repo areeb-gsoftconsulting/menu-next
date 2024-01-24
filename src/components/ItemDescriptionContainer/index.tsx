c24854a1e73e994c9a61ef9884c5e70b734c24d0
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2024-01-24 15:36:52 +0500

    feat: added formated data

--- src/components/ItemDescriptionContainer/index.tsx
+++ src/components/ItemDescriptionContainer/index.tsx
@@ -1,49 +1,112 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { BaseEditor, Descendant, createEditor } from "slate";
 import { Editable, ReactEditor, Slate, withReact } from "slate-react";
-
-type Props = {};
-
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [
-      {
-        text: "You can have it iced or hot, with regular milk or plant based alternative (oat or pea) ",
-      },
-    ],
-  },
-  {
-    type: "paragraph",
-    children: [
-      {
-        text: "Sie können ihn eisgekühlt oder heiß, mit normaler Milch oder einer veganen Alternative (Hafer- oder Erbsenmilch) genießen",
-      },
-    ],
-  },
-];
+import { withHistory } from "slate-history";
 
 const ItemDescriptionContainer = ({ data }: any) => {
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
   const [text, setText] = useState<any>([]);
   const description = async () => {
     console.log("testing 1", data);
     let temp = await JSON.parse(data);
     setText(temp);
   };
+
   useEffect(() => {
     description();
   }, []);
+
   console.log({ text });
-  const [editor] = useState(() => withReact(createEditor()));
   // Render the Slate context.
 
   return text.length == 0 ? (
     <p>Loading...</p>
   ) : (
     <Slate editor={editor} initialValue={text}>
-      <Editable readOnly />
+      <Editable
+        className="editable"
+        style={{
+          border: "none",
+        }}
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        spellCheck
+        autoFocus
+        readOnly
+      />
     </Slate>
   );
+};
+
+const Element = ({ attributes, children, element }) => {
+  const style = { textAlign: element.align };
+  switch (element.type) {
+    case "block-quote":
+      return (
+        <blockquote style={style} {...attributes}>
+          {children}
+        </blockquote>
+      );
+    case "bulleted-list":
+      return (
+        <ul style={style} {...attributes}>
+          {children}
+        </ul>
+      );
+    case "heading-one":
+      return (
+        <h1 style={style} {...attributes}>
+          {children}
+        </h1>
+      );
+    case "heading-two":
+      return (
+        <h2 style={style} {...attributes}>
+          {children}
+        </h2>
+      );
+    case "list-item":
+      return (
+        <li style={style} {...attributes}>
+          {children}
+        </li>
+      );
+    case "numbered-list":
+      return (
+        <ol style={style} {...attributes}>
+          {children}
+        </ol>
+      );
+    default:
+      return (
+        <p style={style} {...attributes}>
+          {children}
+        </p>
+      );
+  }
+};
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
 };
 
 export default ItemDescriptionContainer;
