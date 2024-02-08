8ebf34d876277a37c6d5e7c814f00eda8ebaed83
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2024-02-08 21:59:59 +0500

    feat: delete modal

--- src/components/DeleteAlert/index.tsx
+++ src/components/DeleteAlert/index.tsx
@@ -1,38 +1,52 @@
 import React from "react";
-import { IonAlert, IonButton } from "@ionic/react";
+import {
+  IonAlert,
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonModal,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import styles from "./styles.module.css";
+import { starSharp, add, remove, closeCircleSharp } from "ionicons/icons";
 
 function DeleteAlert({ isOpen, setOpen, onDelete }: any) {
   return (
-    <IonAlert
-      className={styles.alert}
-      subHeader="Are you sure!"
-      message="You want to delete this item from the cart?"
+    <IonModal
+      onDidDismiss={() => setOpen(false)}
+      className={styles.main}
       isOpen={isOpen}
-      onWillDismiss={() => setOpen(false)}
-      buttons={[
-        {
-          text: "Cancel",
-          role: "cancel",
-          handler: () => {
-            console.log("Alert canceled");
-          },
-          cssClass: styles.btn,
-        },
-        {
-          text: "OK",
-          role: "confirm",
-          handler: () => {
-            onDelete();
-          },
-          cssClass: styles.btn,
-        },
-      ]}
-      onDidDismiss={({ detail }) => {
-        setOpen(false);
-        console.log(`Dismissed with role: ${detail.role}`);
-      }}
-    ></IonAlert>
+    >
+      <IonContent className={`ion-padding ${styles.mainContent}`}>
+        <IonRow class="ion-justify-content-between">
+          <IonTitle className={styles.title}></IonTitle>
+          <IonIcon
+            onClick={() => setOpen(false)}
+            className={styles.cancelIcon}
+            icon={closeCircleSharp}
+          ></IonIcon>
+        </IonRow>
+        <h3>Delete Item</h3>
+        <p>You want to delete this item from the cart?</p>
+        <IonRow class="ion-justify-content-center">
+          <IonButton
+            onClick={() => setOpen(false)}
+            className={styles.cancelBtn}
+            mode="ios"
+            color={"dark"}
+          >
+            Cancel
+          </IonButton>
+          <IonButton className={styles.delBtn} onClick={onDelete} mode="ios">
+            Delete
+          </IonButton>
+        </IonRow>
+      </IonContent>
+    </IonModal>
   );
 }
 export default DeleteAlert;
