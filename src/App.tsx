8e115e4a9a61def13d8b2eafca8a6d58b6e80a64
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2024-03-18 15:25:47 +0500

    feat: updated code

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,17 @@ const App: React.FC = () => {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <IonApp>
-          <h1 style={{ color: "red" }}>ertyuio</h1>
+          <IonReactRouter>
+            <Switch>
+              <Route path="/welcome" exact>
+                <WelcomePage />
+              </Route>
+              <Route path="/:userName">
+                <DynamicRouteComponent />
+              </Route>
+              <Redirect from="/" to="/welcome" exact />
+            </Switch>
+          </IonReactRouter>
         </IonApp>
       </PersistGate>
     </Provider>
